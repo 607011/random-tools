33c7c1007b4b23a80253952f302bf90e5b45f7c6
Oliver Lau <oliver.lau@gmail.com>
2009-01-02 14:29:58 +0100

    dist.cmd neu.

---
+++ dist.cmd
@@ -0,0 +1,10 @@
+@SET DSTDIR=randomtools
+@SET ZIPFILE=randomtools-win32.zip
+@SET TOOLDIR=win32-tools
+@IF NOT EXIST %DSTDIR% MKDIR %DSTDIR%
+@ECHO Kopieren der EXE-Dateien ...
+@FOR %%F IN (ct-rng ct-randcam ct-rng-assessor ct-rng-analyzer) DO @COPY /B /Y /V Release\%%F.exe %DSTDIR%\
+@COPY /Y LICENSE.txt %DSTDIR%
+@ECHO Zip'pen der EXE-Dateien in %DSTDIR% ...
+@%TOOLDIR%\zip.exe -9 %ZIPFILE% %DSTDIR%\*
+@%TOOLDIR%\unzip.exe -t -q %ZIPFILE%
