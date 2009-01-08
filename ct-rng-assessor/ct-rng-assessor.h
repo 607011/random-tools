fae3f46fc763d2f1bebcd8741a9ef23ba9bd3606
Oliver Lau <oliver.lau@gmail.com>
2009-01-08 21:14:42 +0100

    ct-rng-assessor/ct-rng-assessor.h hinzugef?gt.

---
+++ ct-rng-assessor/ct-rng-assessor.h
@@ -0,0 +1,23 @@
+// $Id$
+// Copyright (c) 2009 Oliver Lau <ola@ctmagazin.de>, Heise Zeitschriften Verlag.
+// Alle Rechte vorbehalten.
+
+
+#ifndef __CT_RNG_ASSESSOR_H_
+#define __CT_RNG_ASSESSOR_H_
+
+#include <iostream>
+#include <iomanip>
+#include <limits>
+#include <cstdlib>
+#include <cmath>
+#include "tests.h"
+
+#define VERSION "1.0.1"
+
+extern bool quiet;
+extern bool htmlReport;
+extern int verbose;
+
+
+#endif // __CT_RNG_ASSESSOR_H_
